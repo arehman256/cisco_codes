fd568c99949b9a98dd89579d2e315d14ab6edeae
arehman256 <110806797+arehman256@users.noreply.github.com>
2023-09-12 13:02:02 +0500

    telnet.py
    Host = 192.168.7.13
    enable password is cisco

--- device.py
+++ device.py
@@ -1,7 +1,26 @@
-device = {
-        'device_type':'cisco_ios',
-        'ip': IP ,
-        'username': user ,
-        'password': passcode,
-        'port': '22' ,
-}
+import getpass
+import telnetlib
+
+HOST = "192.168.7.13"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+    tn.write(b"enable\n")
+
+
+
+tn.write(b"enable\n")
+tn.read_until(b"Password: ")
+tn.write(b"cisco\n")
+tn.write(b"sh ip int bri\n")
+tn.write(b"exit\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
